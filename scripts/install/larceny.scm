b25bfd5008d7f9cc71e8d9c2a4d6db42e9c7f998
Takashi Kato <ktakashi@ymail.com>
2018-05-18 11:21:53 +0200

    Using rename-file to avoid recompilation of fasl on Larceny

--- scripts/install/larceny.scm
+++ scripts/install/larceny.scm
@@ -58,8 +58,8 @@
       ;; FIXME platform
       (download "linux86")
       (let ((path (scheme-env:find-extracted-directory ".")))
-	(create-directory* install-prefix)
-	(copy-directory path install-prefix))))
+	(delete-directory* install-prefix)
+	(rename-file path install-prefix))))
   (let ((new (scheme-env:binary-path "larceny" real-version)))
     (scheme-env:call-with-script-file new install-prefix "run-larceny"
       (lambda (out)
